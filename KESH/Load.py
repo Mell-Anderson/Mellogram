cebb5d9b81281398ee48a683573f1a3b4907602b
Cris <56567236+Mell-Anderson@users.noreply.github.com>
2022-10-13 11:58:49 +0300

    Delete KESH directory

--- KESH/Load.py
+++
@@ -1,8 +0,0 @@
-import pickle
-from modules.config import *
-
-NameFile = None
-
-def load(NameFile, Password):
-    with open(f'LOG/{NameFile}.pkl') as f:
-        NameFile, Password, BitcoinSell, BitcoinBuy, Ark, Nip, Bn, Mon, Kod, Apple, Sumsung, Microsoft, language, MachineBuy, PassportBuy, MachineSell, MineMoney, Machine, Passport, QuantityBuy, QuantitySell, ANS, InventoryQuantityMachine, InventoryQuantityPassport, MoneyApple, MoneySumsung, MoneyMicrosoft, MoneyApple2, MoneySumsung2, MoneyMicrosoft2, BitcoinBuy2, BitcoinSell2, Machine2, Passport2, Nip2, v3 = pickle.load(f)
